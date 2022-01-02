3c78183dfaba0b0b45124ff27bf670511692ce14
Munir Zikrullaev <munirprog@gmail.com>
2022-01-02 12:50:19 +0300

    implement task service

---
+++ src/main/java/com/mdev/service/TaskService.java
@@ -0,0 +1,27 @@
+package com.mdev.service;
+
+import com.mdev.entity.Task;
+import com.mdev.entity.User;
+import com.mdev.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class TaskService {
+
+    private final UserRepository userRepository;
+
+    public List<Task> getTasks(){
+        var email = SecurityContextHolder.getContext().getAuthentication().getName();
+        var user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User not found")
+        );
+        return user.getTasks();
+    }
+}
