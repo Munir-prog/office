0c70129f746dab321c60d5a661d3dd1880ea0c7b
Munir Zikrullaev <munirprog@gmail.com>
2022-01-02 12:49:14 +0300

    implement project security

---
+++ src/main/java/com/mdev/security/UserDetailsServiceImpl.java
@@ -0,0 +1,25 @@
+package com.mdev.security;
+
+import com.mdev.entity.User;
+import com.mdev.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service("userDetailsServiceImpl")
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        var user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("User doesn't exist")
+        );
+        return SecurityUser.fromUser(user);
+    }
+}
